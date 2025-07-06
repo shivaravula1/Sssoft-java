c13f11f4652c436fb58bba493f1565f55bb86b9a
Shiva Ravula <shivanayudu123@gmail.com>
2025-07-05 23:05:33 -0400

    Shiva Ravula Consultant App

---
+++ Consultant_detail/Consultant_detail/src/test/java/com/example/Consultant_detail/ConsultantDetailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Consultant_detail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultantDetailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
